1de3059c84fa4d7157c67888ac29d4580ceffcfa
Maksim <82227040+ananos548@users.noreply.github.com>
2023-09-24 21:54:04 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,12 @@ from .views import LessonViewSet, ProductViewSet, StatisticsView
 from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
-router.register(r'lessons', LessonViewSet)
+
 router.register(r'product', ProductViewSet)
 
 urlpatterns = [
-    path('statistics', StatisticsView.as_view())
+    path('statistics', StatisticsView.as_view()),
+    path('my_lessons', LessonViewSet.as_view({'get': 'list'}))
 ]
 
 urlpatterns += router.urls
